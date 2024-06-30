c028c629e57b8f556aa1268bb8f90ee08005e91c
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-07-01 02:21:38 +0800

    Done Magic Seven

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:rush) { described_class.new('Rush', 'Labrador', 'White') }
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'When Dog has the same method name to other classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(rush).to_not be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(rush).to be_sleeping
   end
 end
--- spec/12_magic_seven_spec.rb
+++ spec/12_magic_seven_spec.rb
@@ -70,34 +70,75 @@ describe MagicSeven do
   # Write a test for each of the following methods:
 
   describe '#subtract_four' do
+    context 'when the previous step is 16' do
+      it 'returns 12' do
+        previous_step = 16
+        result = game.subtract_four(previous_step)
+        expect(result).to eq(12)
+      end
+    end
   end
 
   describe '#divide_by_two' do
+    context 'when the previous step is 12' do
+      it 'returns 6' do
+        previous_step = 12
+        result = game.divide_by_two(previous_step)
+        expect(result).to eq(6)
+      end
+    end
   end
 
   # The following tests will need you to create new instances of MagicSeven with
   # a specific value for the random_number.
   describe '#subtract_random_number' do
+    random_number = 12
+
+    subject(:new_magic_seven) { described_class.new(random_number) }
+
+    context 'when the random number is 12' do
+      it 'returns 7' do
+        step_one = new_magic_seven.add_nine(random_number)
+        step_two = new_magic_seven.multiply_by_two(step_one)
+        step_three = new_magic_seven.subtract_four(step_two)
+        step_four = new_magic_seven.divide_by_two(step_three)
+        result = new_magic_seven.subtract_random_number(step_four)
+
+        expect(result).to eq(7)
+      end
+    end
   end
 
   # The #play method will always return seven! Test this game, using any
   # integer as the random_number. Update the context with the number.
   describe '#play' do
-    context 'when the random number is ...' do
+    random_number = 20
+    subject(:new_magic_seven) { described_class.new(random_number) }
+
+    context 'when the random number is 20' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(game.play).to eq(7)
       end
     end
 
-    context 'when the random number is ...' do
+    random_number = 16
+    subject(:new_magic_seven) { described_class.new(random_number) }
+
+    context 'when the random number is 16' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(game.play).to eq(7)
       end
     end
 
-    context 'when the random number is ...' do
+    random_number = 4
+    subject(:new_magic_seven) { described_class.new(random_number) }
+
+    context 'when the random number is 4' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(game.play).to eq(7)
       end
     end
   end
